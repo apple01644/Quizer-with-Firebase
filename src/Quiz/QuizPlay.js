a86abc8a5c2c24a53e1b780db6f9d6dd190084cf
윤재상 <banana2692@daum.net>
2020-12-06 00:28:59 +0900

    Fix QuizPlay: Hint appears in order
    Fix QuizPlay: User Answer may appear hint
    Fix QuizPlay: Auto Pass to next question
    Fix QuizPlay: Type .(dot) to get hint
    Feature QuizPlay: style-components

--- src/Quiz/QuizPlay.js
+++ src/Quiz/QuizPlay.js
@@ -1,5 +1,10 @@
 import { Button } from 'react-bootstrap';
-import { Component } from 'react';
+import { Component, createRef } from 'react';
+import {
+  MaskedHint,
+  DocumentViewWithScroll,
+  UserAnswerInput,
+} from './QuizPlay.style';
 
 function formatAnswer(answer, real_answer) {
   let result = answer;
@@ -7,21 +12,12 @@ function formatAnswer(answer, real_answer) {
     result += real_answer[result.length] === ' ' ? '.' : '●';
   return result;
 }
-function shuffleArray(array) {
-  let curId = array.length;
-  while (0 !== curId) {
-    let randId = Math.floor(Math.random() * curId);
-    curId -= 1;
-    let tmp = array[curId];
-    array[curId] = array[randId];
-    array[randId] = tmp;
-  }
-  return array;
-}
 
 class QuizPlay extends Component {
   initialize(first) {
     this.blocks = [];
+    this.quiz_buttons = [];
+    this.quiz_view = createRef();
     const real_answers = [];
     const answers = [];
     const hints = [];
@@ -36,6 +32,7 @@ class QuizPlay extends Component {
       } else if (ch === '>' && !is_text) {
         buffer.toLowerCase();
         this.blocks.push(['button', buffer, answers.length]);
+        this.quiz_buttons.push(createRef());
         real_answers.push(buffer);
         answers.push('');
         hints.push(formatAnswer('', buffer));
@@ -47,11 +44,12 @@ class QuizPlay extends Component {
         buffer = '';
       } else buffer += ch;
     }
+    if (buffer.length > 0) this.blocks.push(['text', buffer]);
     if (first === true) {
       this.state = {
         holding_index: 0,
         real_answers: real_answers,
-        answers: answers,
+        user_answers: answers,
         hints: hints,
         answer_input: '',
       };
@@ -59,7 +57,7 @@ class QuizPlay extends Component {
       this.setState({
         holding_index: 0,
         real_answers: real_answers,
-        answers: answers,
+        user_answers: answers,
         hints: hints,
         answer_input: '',
       });
@@ -76,133 +74,200 @@ class QuizPlay extends Component {
     this.initialize(true);
   }
 
+  setUserAnswer(e) {
+    const answers = Array.from(this.state.user_answers);
+    const hints = Array.from(this.state.hints);
+    const user_answer = e.target.value || '';
+    const real_answer = this.state.real_answers[this.state.holding_index];
+
+    console.log(
+      e.target.value,
+      e.target.value.indexOf('.'),
+      e.target.value.replaceAll('.', '')
+    );
+    if (e.target.value.indexOf('.') !== -1) {
+      e.target.value = e.target.value.replaceAll('.', '');
+      this.getHint();
+      return;
+    }
+
+    if (answers[this.state.holding_index] !== real_answer)
+      answers[this.state.holding_index] = user_answer;
+
+    for (
+      let idx = 0;
+      idx < real_answer.length && idx < user_answer.length;
+      ++idx
+    ) {
+      if (user_answer[idx] !== ' ' && user_answer[idx] === real_answer[idx]) {
+        hints[this.state.holding_index] =
+          hints[this.state.holding_index].substr(0, idx) +
+          real_answer[idx] +
+          hints[this.state.holding_index].substr(idx + 1);
+      }
+    }
+    this.setState({
+      user_answers: answers,
+      answer_input: user_answer,
+      hints: hints,
+    });
+    if (
+      user_answer === real_answer &&
+      this.state.holding_index + 1 < answers.length
+    ) {
+      this.quiz_view.current.scrollTop =
+        this.quiz_buttons[this.state.holding_index + 1].current.offsetTop -
+        this.quiz_view.current.offsetTop;
+      this.setState({
+        holding_index: this.state.holding_index + 1,
+        answer_input: '',
+      });
+    }
+  }
+
+  getHint() {
+    const masks = this.state.hints[this.state.holding_index]
+      .split('')
+      .map((ch, idx) => [ch, idx])
+      .filter(([ch, idx]) => ch === '●')
+      .map(([ch, idx]) => idx);
+    if (masks.length > 0) {
+      let idx = masks[0];
+
+      const hints = Array.from(this.state.hints);
+      if (hints[this.state.holding_index][0] === '●') {
+        idx = 0;
+      }
+
+      hints[this.state.holding_index] =
+        hints[this.state.holding_index].substr(0, idx) +
+        this.state.real_answers[this.state.holding_index][idx] +
+        hints[this.state.holding_index].substr(idx + 1);
+      this.setState({
+        hints: hints,
+      });
+    }
+  }
+
+  passQuestion() {
+    const answers = Array.from(this.state.user_answers);
+    answers[this.state.holding_index] = this.state.real_answers[
+      this.state.holding_index
+    ];
+    this.setState({
+      user_answers: answers,
+      answer_input: this.state.real_answers[this.state.holding_index],
+    });
+
+    if (this.state.holding_index + 1 < answers.length) {
+      this.quiz_view.current.scrollTop =
+        this.quiz_buttons[this.state.holding_index + 1].current.offsetTop -
+        this.quiz_view.current.offsetTop;
+      this.setState({
+        holding_index: this.state.holding_index + 1,
+        answer_input: '',
+      });
+    }
+  }
+
+  didAnswerAllQuestions() {
+    let k = this.state.real_answers.length - 1;
+    while (this.state.real_answers[k] === this.state.user_answers[k] && k >= 0)
+      k -= 1;
+
+    return k === -1;
+  }
+
   render() {
     return (
       <div>
-        {this.blocks.map((pair) => {
-          if (pair[0] === 'text') return pair[1];
-          else if (pair[0] === 'button')
-            return (
-              <Button
-                key={pair[2]}
-                variant={
-                  pair[2] === this.state.holding_index
-                    ? 'outline-primary'
-                    : pair[1] === this.state.answers[pair[2]]
-                    ? 'outline-success'
-                    : 'outline-danger'
-                }
-                size='sm'
-                className='p-0'
-                onClick={() => {
-                  this.setState({
-                    holding_index: pair[2],
-                    answer_input: '',
-                  });
-                }}
-              >
-                {formatAnswer(this.state.answers[pair[2]], pair[1])}
-              </Button>
-            );
-          else if (pair[0] === 'br') return <br />;
-          else return null;
-        })}
-        <br />
-        <div></div>
-        <br />
+        <DocumentViewWithScroll
+          className='border-bottom border-secondary my-2 px-3 text-left'
+          ref={this.quiz_view}
+        >
+          {this.blocks.map((pair) => {
+            if (pair[0] === 'text') return pair[1];
+            else if (pair[0] === 'button') {
+              return (
+                <Button
+                  key={pair[2]}
+                  ref={this.quiz_buttons[pair[2]]}
+                  variant={
+                    pair[2] === this.state.holding_index
+                      ? 'outline-primary'
+                      : pair[1] === this.state.user_answers[pair[2]]
+                      ? 'outline-success'
+                      : 'outline-danger'
+                  }
+                  size='sm'
+                  className='p-0'
+                  onClick={() => {
+                    this.setState({
+                      holding_index: pair[2],
+                      answer_input: '',
+                    });
+                  }}
+                  style={
+                    pair[1] === this.state.user_answers[pair[2]]
+                      ? { 'point-events': 'none' }
+                      : {}
+                  }
+                >
+                  {formatAnswer(this.state.user_answers[pair[2]], pair[1])}
+                </Button>
+              );
+            } else if (pair[0] === 'br') return <br />;
+            else return null;
+          })}
+        </DocumentViewWithScroll>
         <div className='d-flex flex-column justify-content-start mx-3'>
           <Button
             variant='outline-dark'
-            onClick={() => {
-              const masks = this.state.hints[this.state.holding_index]
-                .split('')
-                .map((ch, idx) => [ch, idx])
-                .filter(([ch, idx]) => ch === '●')
-                .map(([ch, idx]) => idx);
-              if (masks.length > 0) {
-                shuffleArray(masks);
-                const idx = masks[0];
-
-                const hints = Array.from(this.state.hints);
-                hints[this.state.holding_index] =
-                  hints[this.state.holding_index].substr(0, idx) +
-                  this.state.real_answers[this.state.holding_index][idx] +
-                  hints[this.state.holding_index].substr(idx + 1);
-                this.setState({
-                  hints: hints,
-                });
-              }
-            }}
+            className='d-flex flex-row justify-content-center'
+            onClick={() => this.getHint()}
           >
             {this.state.hints[this.state.holding_index]
               .split('')
               .map((ch, idx) => {
                 if (
                   this.state.real_answers[this.state.holding_index][idx] ===
-                  this.state.answers[this.state.holding_index][idx]
+                    this.state.user_answers[this.state.holding_index][idx] &&
+                  this.state.real_answers[this.state.holding_index][idx] !== ' '
                 )
                   return (
-                    <a key={idx} className='text-success'>
+                    <div key={idx} className='text-success m-0 p-0'>
                       {ch}
-                    </a>
+                    </div>
                   );
                 else
                   return (
-                    <a key={idx} className='text-danger'>
+                    <MaskedHint key={idx} className='text-danger m-0 p-0'>
                       {ch}
-                    </a>
+                    </MaskedHint>
                   );
               })}
           </Button>
-          <div className='justify-content-center align-items-stretch'>
-            <input
+          <div className='justify-content-between mt-2'>
+            <UserAnswerInput
               className='my-0'
               type='text'
               placeholder='Type answer'
               value={this.state.answer_input}
-              onChange={(e) => {
-                const answers = Array.from(this.state.answers);
-                if (
-                  answers[this.state.holding_index] !==
-                  this.state.real_answers[this.state.holding_index]
-                )
-                  answers[this.state.holding_index] = e.target.value;
-                this.setState({
-                  answers: answers,
-                  answer_input: e.target.value,
-                });
-              }}
+              onChange={(e) => this.setUserAnswer(e)}
             />
-            <Button
-              className='ml-2'
-              variant='warning'
-              onClick={() => {
-                const answers = Array.from(this.state.answers);
-                answers[this.state.holding_index] = this.state.real_answers[
-                  this.state.holding_index
-                ];
-                this.setState({
-                  answers: answers,
-                  answer_input: this.state.real_answers[
-                    this.state.holding_index
-                  ],
-                });
-
-                const able_questions = this.state.answers
-                  .map((answer, idx) => [answer, idx])
-                  .filter(
-                    ([answer, idx]) => answer !== this.state.real_answers[idx]
-                  )
-                  .map(([answer, idx]) => idx);
-
-                if (able_questions.length > 0) {
-                  shuffleArray(able_questions);
-                  this.setState({ holding_index: able_questions[0] });
-                }
-              }}
-            >
-              Pass
-            </Button>
+            {this.state.real_answers !== undefined &&
+            this.didAnswerAllQuestions() ? (
+              <Button
+                variant='success'
+                onClick={() => this.props.goNextQuestion()}
+              >
+                Next
+              </Button>
+            ) : (
+              <Button variant='warning' onClick={() => this.passQuestion()}>
+                Pass
+              </Button>
+            )}
           </div>
         </div>
       </div>
---
+++ src/Quiz/QuizPlay.style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const MaskedHint = styled.div`
+  width: 1rem;
+`;
+
+const DocumentViewWithScroll = styled.div`
+  height: 19rem;
+  overflow-y: scroll;
+`;
+
+const UserAnswerInput = styled.input`
+  width: 16.45rem;
+  padding: 0.375rem 0.75rem;
+  vertical-align: middle;
+`;
+
+export { MaskedHint, DocumentViewWithScroll, UserAnswerInput };
