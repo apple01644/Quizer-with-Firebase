d6e9546154d370904142565f4daabd679f08fecb
윤재상 <banana2692@daum.net>
2020-12-06 00:29:32 +0900

    Feature QuizView: Start Single Quiz & Masking

--- src/Quiz/QuizView.js
+++ src/Quiz/QuizView.js
@@ -2,11 +2,13 @@ import {
   FirebaseDatabaseNode,
   FirebaseDatabaseMutation,
 } from '@react-firebase/database';
+import { createRef } from 'react';
 import { Card, Button } from 'react-bootstrap';
 import { MarkdownReader } from './MarkdownReader';
 const { Component } = require('react');
 
 class QuizView extends Component {
+  markdown_reader = createRef();
   render() {
     const btn_back = (
       <Button
@@ -76,12 +78,35 @@ class QuizView extends Component {
                     </FirebaseDatabaseMutation>
                   </>
                 ) : null}
+                <Button
+                  className='mt-2 ml-2'
+                  variant='info'
+                  size='sm'
+                  onClick={() => {
+                    this.props.setpage('quiz_game', {
+                      list: [this.props.pagedata.idx],
+                    });
+                  }}
+                >
+                  Quiz
+                </Button>
+                <Button
+                  className='mt-2 ml-2'
+                  variant='secondary'
+                  size='sm'
+                  onClick={() => this.markdown_reader.current.hideAll()}
+                >
+                  Mask
+                </Button>
                 <br />
                 <div className='m-3 mh-100'>
                   <Card className='mh-100'>
                     <Card.Body>
                       <Card.Title>{post.title}</Card.Title>
-                      <MarkdownReader value={post.md} />
+                      <MarkdownReader
+                        value={post.md}
+                        ref={this.markdown_reader}
+                      />
                     </Card.Body>
                   </Card>
                 </div>
