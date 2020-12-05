f833f4bb670e636e9a28714fa2162b03e67604f7
윤재상 <banana2692@daum.net>
2020-12-06 00:31:10 +0900

    Feature markdownReader: Masking

--- src/Quiz/MarkdownReader.js
+++ src/Quiz/MarkdownReader.js
@@ -1,23 +1,43 @@
 import { Button } from 'react-bootstrap';
-const { Component } = require('react');
+import { Component, createRef } from 'react';
+
+class FlipButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hide: false };
+  }
+
+  render() {
+    return (
+      <Button
+        variant='outline-dark'
+        size='sm'
+        className={'p-0 ' + (this.state.hide ? ' text-white' : '')}
+        onClick={() => this.setState({ hide: !this.state.hide })}
+      >
+        {this.props.value}
+      </Button>
+    );
+  }
+}
 
 class MarkdownReader extends Component {
   constructor(props) {
     super(props);
     this.dom = [];
+    this.buttons = [];
     let buffer = '';
     let is_text = true;
+    let idx = 0;
     for (const ch of this.props.value) {
       if (ch === '<' && is_text) {
         this.dom.push(buffer);
         buffer = '';
         is_text = false;
       } else if (ch === '>' && !is_text) {
-        this.dom.push(
-          <Button variant='outline-dark' size='sm' className='p-0'>
-            {buffer}
-          </Button>
-        );
+        let ref = createRef();
+        this.dom.push(<FlipButton key={idx} value={buffer} ref={ref} />);
+        this.buttons.push(ref);
         buffer = '';
         is_text = true;
       } else if (ch === '\n' && is_text) {
@@ -25,6 +45,14 @@ class MarkdownReader extends Component {
         this.dom.push(<br />);
         buffer = '';
       } else buffer += ch;
+      idx += 1;
+    }
+    if (buffer.length > 0) this.dom.push(buffer);
+  }
+
+  hideAll() {
+    for (const ref of this.buttons) {
+      ref.current.setState({ hide: true });
     }
   }
 
