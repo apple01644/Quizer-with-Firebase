d9cf2aa3091f94661b6c89c6a3df888e96d53d1c
윤재상 <banana2692@daum.net>
2021-01-31 23:12:14 +0900

    Delete deprecated codes from App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import './App.css';
 import { Component } from 'react';
 import { Button } from 'react-bootstrap';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  withRouter,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { QuizHome } from './Quiz';
-import { MarkdownReaderV2 } from './Quiz/MarkdownReader';
 import { QuizView } from './Quiz/QuizView';
 import { QuizNew } from './Quiz/QuizNew';
 import { QuizEdit } from './Quiz/QuizEdit';
@@ -16,8 +10,6 @@ import { QuizEdit } from './Quiz/QuizEdit';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
-
-import { Form } from 'react-bootstrap';
 
 const firebaseConfig = require('./FirebaseConfig.json');
 
@@ -65,43 +57,6 @@ class TopMenu extends Component {
     );
   }
 }
-
-class Editor extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      post_data: { md: '', category: null, title: '', uid: null },
-    };
-  }
-
-  componentDidMount() {
-    const params = new URLSearchParams(this.props.location.search);
-    const page_id = params.get('page');
-    firebase
-      .database()
-      .ref(`/posts/${page_id}`)
-      .once('value')
-      .then((s) => {
-        const post_db = s.val();
-        this.setState({ post_data: post_db });
-      });
-  }
-
-  render() {
-    return (
-      <div className='m-3 flex-fill d-flex flex-row w-100 h-100 justify-content-around text-left'>
-        <Form.Control
-          as='textarea'
-          className='mx-3 px-3 py-2'
-          style={{ resize: 'None' }}
-          onChange={(e) => this.setState({ data: e.target.value })}
-        />
-        <MarkdownReaderV2 data={this.state.post_data.md} />
-      </div>
-    );
-  }
-}
-const EditorwithRouter = withRouter(Editor);
 
 class App extends Component {
   constructor(props) {
