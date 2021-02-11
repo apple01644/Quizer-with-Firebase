82da15a79c344f1a52f8d655f9ba0d7fa0233389
윤재상 <banana2692@daum.net>
2021-02-11 13:00:43 +0900

    Feature Blank in Table on MarkdownReaderV2.js
    Fix Disappearing chracter after new line

--- src/Quiz/MarkdownReader.js
+++ src/Quiz/MarkdownReader.js
@@ -21,16 +21,17 @@ class FlipButton extends Component {
         )}
         className={
           'align-self-center p-0 m-0 bg-white ' +
-          (this.props.ParentState.content_array[this.props.idx].hide
+          (this.props.ParentState.blank_array[this.props.blank_array_idx].hide
             ? 'text-white'
             : 'text-dark')
         }
         onClick={() => {
-          const content_array = this.props.ParentState.content_array;
-          content_array[this.props.idx].hide = !content_array[this.props.idx]
-            .hide;
+          const blank_array = this.props.ParentState.blank_array;
+          blank_array[this.props.blank_array_idx].hide = !blank_array[
+            this.props.blank_array_idx
+          ].hide;
           this.props.setParentState({
-            content_array: content_array,
+            blank_array: blank_array,
           });
         }}
         children={this.props.value}
@@ -56,7 +57,7 @@ class QuizButton extends Component {
     return (
       <Button
         as='p'
-        ref={this.props.ParentState.blank_ref_set[this.props.idx]}
+        ref={this.props.ParentState.blank_ref_set[this.props.blank_array_idx]}
         variant={this.isSolved() ? 'outline-success' : 'outline-danger'}
         size='sm'
         style={Object.assign({ boxShadow: 'none' }, this.props.style)}
@@ -66,7 +67,7 @@ class QuizButton extends Component {
             : 'text-success'
         } ${this.props.selected ? 'bg-warning focus' : 'bg-white'}`}
         onClick={() => {
-          this.props.setCursor(this.props.idx);
+          this.props.setCursor(this.props.blank_array_idx);
         }}
         children={
           this.isSolved()
@@ -88,78 +89,155 @@ const CONTENT_TYPE = {
 class MarkdownReaderV2 extends Component {
   constructor(props) {
     super(props);
-    this.state = { content_array: [], blank_ref_set: {}, prev_cursor: 0 };
+    this.state = {
+      content_array: [],
+      blank_array: [],
+      blank_ref_set: {},
+      prev_cursor: 0,
+    };
+
+    this.content = undefined;
   }
 
   hideAll() {
-    const content_array = Array.from(this.state.content_array);
-    for (const idx in content_array) {
-      if (content_array[idx].type === CONTENT_TYPE.BLANK)
-        content_array[idx].hide = true;
+    const blank_array = Array.from(this.state.blank_array);
+    for (const idx in blank_array) {
+      blank_array[idx].hide = true;
     }
-    this.setState({ content_array: content_array });
+    this.setState({ blank_array: blank_array });
   }
 
-  parseStyleSign(value, style) {
+  parseStyleSign(value, style, blank_array_idx, set_blank_array_idx) {
     const text = value.split('');
-    const styled_text_array = [];
+    const content_array = [];
     let buffer = '';
     let option_italic = false;
     let option_bold = false;
     let option_underline = false;
     let option_del = false;
 
-    const flushBuffer = () => {
-      styled_text_array.push({
-        options: {
-          italic: option_italic,
-          bold: option_bold,
-          underline: option_underline,
-          del: option_del,
-        },
-        value: buffer,
-      });
+    const flushBuffer = (type) => {
+      if (type === CONTENT_TYPE.TEXT) {
+        content_array.push({
+          options: {
+            italic: option_italic,
+            bold: option_bold,
+            underline: option_underline,
+            del: option_del,
+          },
+          value: buffer,
+          type: type,
+        });
+      } else if (type === CONTENT_TYPE.BLANK) {
+        content_array.push({
+          value: buffer,
+          type: type,
+          blank_array_idx: blank_array_idx++,
+        });
+      } else {
+        content_array.push({
+          options: {
+            italic: option_italic,
+            bold: option_bold,
+            underline: option_underline,
+            del: option_del,
+          },
+          value: `Unknown type ${type}`,
+          type: CONTENT_TYPE.TEXT,
+        });
+      }
       buffer = '';
     };
-
     for (const ch of text) {
       if (ch === '#') {
-        flushBuffer();
+        flushBuffer(CONTENT_TYPE.TEXT);
         option_bold = !option_bold;
         option_underline = !option_underline;
       } else if (ch === '@') {
-        flushBuffer();
+        flushBuffer(CONTENT_TYPE.TEXT);
         option_bold = !option_bold;
       } else if (ch === '\\') {
-        flushBuffer();
+        flushBuffer(CONTENT_TYPE.TEXT);
         option_italic = !option_italic;
       } else if (ch === '_') {
-        flushBuffer();
+        flushBuffer(CONTENT_TYPE.TEXT);
         option_underline = !option_underline;
       } else if (ch === ';') {
-        flushBuffer();
+        flushBuffer(CONTENT_TYPE.TEXT);
         option_del = !option_del;
+      } else if (ch === '<') {
+        flushBuffer(CONTENT_TYPE.TEXT);
+      } else if (ch === '>') {
+        if (buffer.length > 0) flushBuffer(CONTENT_TYPE.BLANK);
       } else buffer += ch;
     }
-    if (buffer.length > 0) flushBuffer();
+    if (buffer.length > 0) flushBuffer(CONTENT_TYPE.TEXT);
 
-    return (
+    const jsx_result = (
       <div className='d-flex' style={style}>
-        {styled_text_array.map((styled_text) => {
-          let result = <p children={styled_text.value} className='m-0' />;
-          if (styled_text.options.italic === true)
-            result = <em children={result} />;
-          if (styled_text.options.bold === true)
-            result = <strong children={result} />;
-          if (styled_text.options.underline === true)
-            result = <u children={result} />;
-          if (styled_text.options.del === true)
-            result = <del children={result} />;
+        {content_array.map((content, idx) => {
+          if (content.type === CONTENT_TYPE.BLANK) {
+            return this.buildBlank(idx, content);
+          } else {
+            let result = (
+              <p key={idx} children={content.value} className='m-0' />
+            );
+            if (content.options.italic === true)
+              result = <em key={idx} children={result} />;
+            if (content.options.bold === true)
+              result = <strong key={idx} children={result} />;
+            if (content.options.underline === true)
+              result = <u key={idx} children={result} />;
+            if (content.options.del === true)
+              result = <del key={idx} children={result} />;
 
-          return result;
+            return result;
+          }
         })}
       </div>
     );
+    if (set_blank_array_idx !== undefined) set_blank_array_idx(blank_array_idx);
+    return jsx_result;
+  }
+
+  buildBlank(idx, content) {
+    if (this.props.quiz_mode !== true) {
+      return (
+        <FlipButton
+          key={idx}
+          blank_array_idx={content.blank_array_idx}
+          ParentState={this.state}
+          setParentState={(e) => this.setState(e)}
+          value={content.value}
+          style={{
+            fontSize: `${content.fontSize_rem}rem`,
+          }}
+        />
+      );
+    } else {
+      return (
+        <QuizButton
+          key={idx}
+          blank_array_idx={content.blank_array_idx}
+          ParentState={this.state}
+          setParentState={(e) => this.setState(e)}
+          value={content.value}
+          user_value={this.props.quiz_data[content.blank_array_idx].user_value}
+          selected={content.blank_array_idx === this.props.quiz_data.cursor}
+          setCursor={(blank_array_idx) => {
+            const quiz_data = this.props.quiz_data;
+            quiz_data.cursor = blank_array_idx;
+            this.props.setParentState({
+              quiz_data: quiz_data,
+            });
+            this.props.onUpdateRealAnswer();
+          }}
+          style={{
+            fontSize: `${content.fontSize_rem}rem`,
+          }}
+        />
+      );
+    }
   }
 
   buildJSX(idx, content) {
@@ -167,7 +245,7 @@ class MarkdownReaderV2 extends Component {
       case CONTENT_TYPE.TEXT:
         if (isNumber(content.value[0]) || isHeaderSign(content.value[0])) {
           return (
-            <p
+            <div
               key={idx}
               className='font-weight-bold mb-0'
               style={{
@@ -175,7 +253,7 @@ class MarkdownReaderV2 extends Component {
               }}
             >
               {this.parseStyleSign(content.value)}
-            </p>
+            </div>
           );
         } else {
           return (
@@ -201,84 +279,72 @@ class MarkdownReaderV2 extends Component {
           />
         );
       case CONTENT_TYPE.BLANK:
-        if (this.props.quiz_mode !== true) {
-          return (
-            <FlipButton
-              key={idx}
-              idx={idx}
-              ParentState={this.state}
-              setParentState={(e) => this.setState(e)}
-              value={content.value}
-              style={{
-                fontSize: `${content.fontSize_rem}rem`,
-              }}
-            />
-          );
-        } else {
-          return (
-            <QuizButton
-              idx={idx}
-              ParentState={this.state}
-              setParentState={(e) => this.setState(e)}
-              value={content.value}
-              user_value={this.props.quiz_data[idx].user_value}
-              selected={idx === this.props.quiz_data.cursor}
-              setCursor={(idx) => {
-                const quiz_data = this.props.quiz_data;
-                quiz_data.cursor = idx;
-                this.props.setParentState({
-                  quiz_data: quiz_data,
-                });
-                this.props.onUpdateRealAnswer();
-              }}
-              style={{
-                fontSize: `${content.fontSize_rem}rem`,
-              }}
-            />
-          );
-        }
+        return this.buildBlank(idx, content);
       case CONTENT_TYPE.TABLE: {
+        let blank_array_idx = content.blank_array_idx;
         return (
-          <table>
-            {content.value.split('\n').map((row) => {
-              return (
-                <tr>
-                  {row.split('|').map((block) => {
-                    console.log(block, block[0] === ':');
-                    if (block[0] === ':') {
-                      return (
-                        <td
-                          className='border'
-                          style={{ whiteSpace: 'nowrap' }}
-                          children={this.parseStyleSign(block.substr(1), {
-                            justifyContent: 'left',
-                          })}
-                        />
-                      );
-                    } else if (block[0] === ';') {
-                      return (
-                        <td
-                          className='border'
-                          style={{ whiteSpace: 'nowrap' }}
-                          children={this.parseStyleSign(block.substr(1), {
-                            justifyContent: 'right',
-                          })}
-                        />
-                      );
-                    } else
-                      return (
-                        <td
-                          className='border'
-                          style={{ whiteSpace: 'nowrap' }}
-                          children={this.parseStyleSign(block, {
-                            justifyContent: 'center',
-                          })}
-                        />
-                      );
-                  })}
-                </tr>
-              );
-            })}
+          <table key={idx}>
+            <tbody>
+              {content.value.split('\n').map((row, row_idx) => {
+                return (
+                  <tr key={row_idx}>
+                    {row.split('|').map((block, col_idx) => {
+                      if (block[0] === ':') {
+                        return (
+                          <td
+                            key={col_idx}
+                            className='border'
+                            style={{ whiteSpace: 'nowrap' }}
+                            children={this.parseStyleSign(
+                              block.substr(1),
+                              {
+                                justifyContent: 'left',
+                              },
+                              blank_array_idx,
+                              (new_blank_array_idx) =>
+                                (blank_array_idx = new_blank_array_idx)
+                            )}
+                          />
+                        );
+                      } else if (block[0] === ';') {
+                        return (
+                          <td
+                            key={col_idx}
+                            className='border'
+                            style={{ whiteSpace: 'nowrap' }}
+                            children={this.parseStyleSign(
+                              block.substr(1),
+                              {
+                                justifyContent: 'right',
+                              },
+                              blank_array_idx,
+                              (new_blank_array_idx) =>
+                                (blank_array_idx = new_blank_array_idx)
+                            )}
+                          />
+                        );
+                      } else
+                        return (
+                          <td
+                            key={col_idx}
+                            className='border'
+                            style={{ whiteSpace: 'nowrap' }}
+                            children={this.parseStyleSign(
+                              block,
+                              {
+                                justifyContent: 'center',
+                              },
+                              blank_array_idx,
+                              (new_blank_array_idx) =>
+                                (blank_array_idx = new_blank_array_idx)
+                            )}
+                          />
+                        );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
           </table>
         );
       }
@@ -295,7 +361,8 @@ class MarkdownReaderV2 extends Component {
       LINE_BREAK: 3,
     };
     let content_array = [];
-    let quiz_data = { cursor: null };
+    let blank_array = [];
+    let quiz_data = { cursor: 0 };
     let blank_ref_set = {};
     let buffer = '';
     let lex = LEX.TEXT;
@@ -303,6 +370,8 @@ class MarkdownReaderV2 extends Component {
     let start_of_line = true;
     let fontSize_rem = 1;
     let line_value = '';
+    let small_buffer = '';
+    let start_blank_array_idx = 0;
 
     function setLex(new_lex) {
       lex = new_lex;
@@ -329,13 +398,13 @@ class MarkdownReaderV2 extends Component {
           if (hasBuffer) append_text(popBuffer());
           setLex(LEX.BLANK);
           break;
-
         case '\n':
           if (hasBuffer) append_text(popBuffer());
           setLex(LEX.LINE_BREAK);
           break;
         case '{':
           if (hasBuffer) append_text(popBuffer());
+          small_buffer = '';
           setLex(LEX.TABLE);
           break;
 
@@ -350,18 +419,22 @@ class MarkdownReaderV2 extends Component {
           if (hasBuffer) {
             const value = popBuffer();
             if (this.props.quiz_mode === true) {
-              if (quiz_data.cursor === null) quiz_data.cursor = idx;
-              quiz_data[idx] = {
+              quiz_data[blank_array.length] = {
                 user_value: '',
                 value: value,
               };
-              blank_ref_set[idx] = createRef();
+              blank_ref_set[blank_array.length] = createRef();
             }
 
             content_array.push({
               idx: idx++,
+              blank_array_idx: blank_array.length,
               type: CONTENT_TYPE.BLANK,
               fontSize_rem: fontSize_rem,
+              value: value,
+            });
+            blank_array.push({
+              idx: blank_array.length,
               value: value,
               hide: false,
             });
@@ -386,18 +459,53 @@ class MarkdownReaderV2 extends Component {
             size: buffer.length,
           });
           buffer = '';
-          if (ch === '{') setLex(LEX.TABLE);
-          else setLex(LEX.TEXT);
+          if (ch === '{') {
+            small_buffer = '';
+            setLex(LEX.TABLE);
+          } else {
+            buffer = ch;
+            setLex(LEX.TEXT);
+          }
       }
     };
 
     const lex_table = (ch, hasBuffer) => {
+      if (!hasBuffer) start_blank_array_idx = blank_array.length;
       switch (ch) {
+        case '<':
+          small_buffer = '<';
+          buffer += ch;
+          break;
+        case '>':
+          if (small_buffer[0] === '<' && small_buffer.length > 1) {
+            const value = small_buffer.substr(1);
+            if (this.props.quiz_mode === true) {
+              quiz_data[blank_array.length] = {
+                user_value: '',
+                value: value,
+              };
+              blank_ref_set[blank_array.length] = createRef();
+            }
+
+            blank_array.push({
+              idx: blank_array.length,
+              value: value,
+              hide: false,
+            });
+          }
+          buffer += ch;
+          break;
+        case '|':
+          small_buffer = '';
+          buffer += ch;
+          break;
+
         case '}':
           content_array.push({
             idx: idx++,
             type: CONTENT_TYPE.TABLE,
             value: buffer,
+            blank_array_idx: start_blank_array_idx,
           });
           buffer = '';
           setLex(LEX.TEXT);
@@ -405,9 +513,9 @@ class MarkdownReaderV2 extends Component {
 
         default:
           buffer += ch;
+          small_buffer += ch;
       }
     };
-
     for (const ch of this.props.data) {
       const hasBuffer = buffer.length > 0;
       if (lex === LEX.TEXT) lex_text(ch, hasBuffer);
@@ -453,11 +561,13 @@ class MarkdownReaderV2 extends Component {
         this.props.onUpdateRealAnswer()
       );
       this.setState({
+        blank_array: Array.from(blank_array),
         content_array: Array.from(content_array),
         blank_ref_set: blank_ref_set,
       });
     } else {
       this.setState({
+        blank_array: Array.from(blank_array),
         content_array: Array.from(content_array),
       });
     }
@@ -504,15 +614,12 @@ class MarkdownReaderV2 extends Component {
 
   resetQuiz() {
     const quiz_data = this.props.quiz_data;
-    quiz_data.cursor = null;
-    this.state.content_array.forEach((content, idx) => {
-      if (content.type === CONTENT_TYPE.BLANK) {
-        if (quiz_data.cursor === null) quiz_data.cursor = idx;
-        quiz_data[idx] = {
-          user_value: '',
-          value: content.value,
-        };
-      }
+    quiz_data.cursor = 0;
+    this.state.blank_array.forEach((blank, blank_array_idx) => {
+      quiz_data[blank_array_idx] = {
+        user_value: '',
+        value: blank.value,
+      };
     });
     this.props.setParentState({ quiz_data: quiz_data });
     this.props.onUpdateRealAnswer();
